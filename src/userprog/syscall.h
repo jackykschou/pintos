46acd244ebdc903162de7596cc96f40dc829aa56
onealm <melanie_oneal@yahoo.com>
2013-12-03 15:37:06 -0600

    Update syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -22,4 +22,11 @@ void seek (int fd, unsigned position);
 unsigned tell (int fd);
 void close (int fd);
 
+/* System call declarations for project 4 */ 
+bool chdir (const char *dir);
+bool mkdir (const char *dir);
+bool readdir (int fd, char *name);
+bool isdir (int fd);
+int inumber (int fd);
+
 #endif /* userprog/syscall.h */
